4a98df2642228b8627b5926436652debc3d3f41b
Mohamed Labib <lebaz20@gmail.com>
2018-07-12 09:31:36 +0200

    Use routes to access application

--- authenticatedProducts/src/index.js
+++ authenticatedProducts/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
 import App from './components/app';
 
@@ -10,7 +11,11 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/products" component={App} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector('.container'),
 );
--- authenticatedProducts/webpack.config.js
+++ authenticatedProducts/webpack.config.js
@@ -21,5 +21,6 @@ module.exports = {
   },
   devServer: {
     contentBase: './',
+    historyApiFallback: true,
   },
 };
